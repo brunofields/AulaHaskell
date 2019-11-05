9059f4bd688222cb04e2c8a20cef875a4f007be2
Bruno Campos <brcampos@everis.com>
2019-11-05 21:34:18 -0200

    fixing route again

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -1,26 +1,26 @@
-    {-# LANGUAGE NoImplicitPrelude #-}
-    {-# LANGUAGE OverloadedStrings #-}
-    {-# LANGUAGE TemplateHaskell #-}
-    {-# LANGUAGE MultiParamTypeClasses #-}
-    {-# LANGUAGE TypeFamilies #-}
-    -- | Common handler functions.
-    module Handler.Common where
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+-- | Common handler functions.
+module Handler.Common where
 
-    import Data.FileEmbed (embedFile)
-    import Import
+import Data.FileEmbed (embedFile)
+import Import
 
-    -- These handlers embed files in the executable at compile time to avoid a
-    -- runtime dependency, and for efficiency.
+-- These handlers embed files in the executable at compile time to avoid a
+-- runtime dependency, and for efficiency.
 
-    getFaviconR :: Handler TypedContent
-    getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
-                    return $ TypedContent "image/x-icon"
-                            $ toContent $(embedFile "config/favicon.ico")
+getFaviconR :: Handler TypedContent
+getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
+                 return $ TypedContent "image/x-icon"
+                        $ toContent $(embedFile "config/favicon.ico")
 
-    getRobotsR :: Handler TypedContent
-    getRobotsR = return $ TypedContent typePlain
-                        $ toContent $(embedFile "config/robots.txt")
+getRobotsR :: Handler TypedContent
+getRobotsR = return $ TypedContent typePlain
+                    $ toContent $(embedFile "config/robots.txt")
 
-    getAdsR :: Handler TypedContent
-    getAdsR = return $ TypedContent typePlain
-                        $ toContent $(embedFile "config/ads.txt")
+getAdsR :: Handler TypedContent
+getAdsR = return $ TypedContent typePlain
+                    $ toContent $(embedFile "config/ads.txt")                    
